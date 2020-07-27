f1df6d0b31a13446afe6dbfddd7b69ae40372206
Angelika Stefan <anmaste92@gmail.com>
2020-07-27 16:49:16 +0200

    add data plot and change prior plot

---
+++ R/plot_datasets.R
@@ -0,0 +1,37 @@
+#### Descriptive plots of datasets ####
+
+plotdata <- function(WetzelsEtAl, BoscoEtAl, seed=1234){
+
+  grDevices::pdf(file = "../Figures/plot_data.pdf", width = 9.2, height=5.2)
+
+  par(mfrow=c(1,2))
+  # Wetzels et al
+  plot(WetzelsEtAl$effectsize, WetzelsEtAl$N1,
+       pch=19, col=scales::alpha("grey26", 0.9),
+       xlab="", ylab="", bty="l")
+  mtext("Sample Size (per group)", side=2, cex=1.2, line=3)
+  mtext(bquote("Effect Size "*delta), side=1, cex=1.2, line=3)
+  mtext("Data: Wetzels et. al (2011)", side=3, cex=1.5, line=1.5)
+
+  # Bosco et al
+
+  ## reduced dataset
+  set.seed(seed)
+  studysample <- sample(unique(BoscoEtAl$studyID), 855, replace = F)
+  ids <- NULL
+  for(i in studysample){
+    dat <- BoscoEtAl[BoscoEtAl$studyID == i,]
+    ids[which(studysample==i)] <- dat$RowID[sample(length(dat$RowID), 1)]
+  }
+  BoscoEtAlRed <- BoscoEtAl[BoscoEtAl$RowID %in% ids,]
+
+  ## plot
+  plot(BoscoEtAlRed$r, BoscoEtAlRed$N,
+       pch=19, col=scales::alpha("grey26", 0.9),
+       xlab="", ylab="", bty="l")
+  mtext("Sample Size (per group)", side=2, cex=1.2, line=3)
+  mtext(bquote("Effect Size "*rho), side=1, cex=1.2, line=3)
+  mtext("Data: Bosco et. al (2015)", side=3, cex=1.5, line=1.5)
+
+  grDevices::dev.off()
+}
--- R/plot_priors.R
+++ R/plot_priors.R
@@ -3,40 +3,34 @@
 #' Plot Prior Distributions for t-test
 #' @param ExpertsPriors_tTest Expert prior dataset for t-test (see /data-raw)
 #' @param ylim Vector of length 2; limits of the y axis
+#' @param disttype Normal distribution ("norm") or t-distribution ("t")
 #' @import ggsci
 #' @import graphics
 
-plotpriors_t <- function(ExpertsPriors_tTest, ylim){
+plotpriors_t <- function(ExpertsPriors_tTest, ylim, disttype){
 
-  grDevices::pdf(file = "../Figures/plot_priors_tTest.pdf",
+  grDevices::pdf(file = paste0("../Figures/plot_priors_tTest_", disttype, ".pdf"),
                  width = 8, height=9)
 
-  cols <- ggsci::pal_aaas("default")(9)
-  expert_name <- c("Social 1", "Social 2", "Neuro 1", "Neuro 2",
-                   "Developmental 1", "Developmental 2")
+  cols <- ggsci::pal_uchicago("light")(9)[c(1:5,9)]
+  expert_name <- c("Expert 1: Social", "Expert 2: Social", "Expert 3: Neuro", "Expert 4: Neuro ",
+                   "Expert 5: Developmental", "Expert 6: Developmental")
 
   graphics::par(mfrow=c(3,2), mar = c(5, 5, 2, 2.1), oma = c(4, 1, 1, 0))
 
   for(i in 1:nrow(ExpertsPriors_tTest)){
     priors <- as.numeric(ExpertsPriors_tTest[i, c(23:27, 29:33)])
     pts <- seq(0, 1, by = 0.001)
-    plot(pts, stats::dnorm(pts, priors[1], priors[2]), ylab = "Density", xlab = bquote(delta),
-         bty="l", type = "l", lwd=2,col=cols[3], ylim = ylim, lty="dotted", las=1,
-         cex.axis=1.5, cex.lab=1.7)
-    graphics::points(pts, dtss(pts, priors[3], priors[4], priors[5]), type = "l", lwd=2,
-           col=cols[4], lty="dotted")
-    graphics::points(pts, stats::dnorm(pts, priors[6], priors[7]), type = "l", lwd=2,
-           col=cols[3])
-    graphics::points(pts, dtss(pts, priors[8], priors[9], priors[10]), type = "l", lwd=2,
-           col=cols[4])
+    if(disttype=="norm"){
+      yval <- stats::dnorm(pts, priors[6], priors[7])
+    } else if (disttype=="t"){
+      yval <- dtss(pts, priors[8], priors[9], priors[10])
+    }
+    plot(pts, yval, ylab = "Density", xlab = bquote(delta),
+         bty="l", type = "l", lwd=2, ylim = ylim, las=1,
+         cex.axis=1.5, cex.lab=1.7, col=cols[i])
     graphics::mtext(expert_name[i], cex=1.2)
   }
-
-  graphics::legend(x = -0.9, y = -2.5,
-         legend = c("t-distribution ", "normal distribution"),
-         lty = c("solid", "solid"), bty = "n", horiz = TRUE, lwd=c(2,2),
-         xpd = NA, col = cols[c(4,3)], adj = 0,
-         text.width = c(0.40, 0.40), cex=1.5)
 
   grDevices::dev.off()
 
@@ -53,20 +47,19 @@ plotpriors_cor <- function(ExpertsPriors_cor, ylim){
   grDevices::pdf(file = "../Figures/plot_priors_cor.pdf",
                  width = 8, height=9)
 
-  cols <- ggsci::pal_aaas("default")(9)
-  expert_name <- c("Social 1", "Social 2", "Neuro 1", "Neuro 2",
-                   "Developmental 1", "Developmental 2")
+  cols <- ggsci::pal_uchicago("light")(9)[c(1:5,9)]
+  expert_name <- c("Expert 1: Social", "Expert 2: Social", "Expert 3: Neuro", "Expert 4: Neuro ",
+                   "Expert 5: Developmental", "Expert 6: Developmental")
 
   graphics::par(mfrow=c(3,2), mar = c(5, 5, 2, 2.1), oma = c(1, 1, 1, 0))
 
-  for(i in 1:nrow(ExpertsPriors_tTest)){
-    priors <- as.numeric(ExpertsPriors_cor[i, c(23:24, 26:27)])
+  for(i in 1:nrow(ExpertsPriors_cor)){
+    priors <- as.numeric(ExpertsPriors_cor[i, c(26:27)])
     pts <- seq(0, 1, by = 0.001)
-    plot(pts, stats::dbeta(pts, priors[1], priors[2]), ylab = "Density", xlab = bquote(rho),
-         bty="l", type = "l", lwd=2,col=cols[1], ylim = ylim, lty="dotted", las=1,
+    yval <- stats::dbeta(pts, priors[1], priors[2])
+    plot(pts, yval, ylab = "Density", xlab = bquote(rho),
+         bty="l", type = "l", lwd=2,col=cols[i], ylim = ylim, las=1,
          cex.axis=1.5, cex.lab=1.7)
-    graphics::points(pts, stats::dbeta(pts, priors[3], priors[4]), type = "l", lwd=2,
-           col=cols[1], lty="solid")
     graphics::mtext(expert_name[i], cex=1.2)
   }
 
